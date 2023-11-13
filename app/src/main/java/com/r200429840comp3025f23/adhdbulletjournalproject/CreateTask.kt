81197197c216fefa8ee0eee8b1c390d646c36936
Raymund <71455069+RaymundRama@users.noreply.github.com>
2023-11-12 21:22:23 -0500

    connected create task to database to submit data to database

--- app/src/main/java/com/r200429840comp3025f23/adhdbulletjournalproject/CreateTask.kt
+++ app/src/main/java/com/r200429840comp3025f23/adhdbulletjournalproject/CreateTask.kt
@@ -3,7 +3,9 @@ package com.r200429840comp3025f23.adhdbulletjournalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import com.r200429840comp3025f23.adhdbulletjournalproject.databinding.ActivityCreateTaskBinding
 import com.r200429840comp3025f23.adhdbulletjournalproject.databinding.ActivityMainBinding
 
@@ -20,10 +22,32 @@ class CreateTask : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
+        //submitting task data to cloud firestore database
         binding.createTaskSubmitBtn.setOnClickListener {
-            if (binding.createTaskTitleEditText.text.toString().isNotEmpty() &&
-                binding.createTextNotesMultilineText.toString().isNotEmpty())
+            var taskTitle = binding.createTaskTitleEditText.text.toString()
+            var taskNotes = binding.createTextNotesMultilineText.toString()
+            var taskDate = binding.createTaskDateEditText.toString()
+
+
+            if (taskTitle.isNotEmpty() &&
+                taskNotes.isNotEmpty())
             {
+                //create an instance of a task
+                var task = Task(taskDate, taskTitle, taskNotes)
+
+                // connect to Firebase-Firestore database
+                val db = FirebaseFirestore.getInstance().collection("tasks")
+
+                // get a unique ID from firestore
+                task.id = db.document().getId()
+
+                // save new Task object to the DB using a unique ID
+                db.document().set(task)
+                    .addOnSuccessListener { Toast.makeText(this, "DB Updated", Toast.LENGTH_LONG).show()}
+                    .addOnFailureListener {exception ->
+                        Toast.makeText(this, "Error Writing to DB",Toast.LENGTH_LONG).show()
+                        Log.w("DB issue", exception.localizedMessage)
+                    }
 
             }
             else
